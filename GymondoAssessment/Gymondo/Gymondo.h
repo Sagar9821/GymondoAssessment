2f3ee95360c1eabe9d3f8c7c7984e67b18946247
Sagar Prajapati <sagarprajapati.2198@gmail.com>
2024-01-19 13:14:19 +0530

    Added Test plan and Framework

---
+++ GymondoAssessment/Gymondo/Gymondo.h
@@ -0,0 +1,18 @@
+//
+//  Gymondo.h
+//  Gymondo
+//
+//  Created by psagc on 19/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Gymondo.
+FOUNDATION_EXPORT double GymondoVersionNumber;
+
+//! Project version string for Gymondo.
+FOUNDATION_EXPORT const unsigned char GymondoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gymondo/PublicHeader.h>
+
+
